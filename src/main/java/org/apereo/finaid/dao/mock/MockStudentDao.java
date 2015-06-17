72b1ea72de45355c8d4b0b4352977634c753e283
KaJuan Johnson <kdjohnson23@oakland.edu>
2015-06-17 11:38:42 -0400

    Successful Build

--- src/main/java/org/apereo/finaid/dao/mock/MockStudentDao.java
+++ src/main/java/org/apereo/finaid/dao/mock/MockStudentDao.java
@@ -1,14 +1,16 @@
 package org.apereo.finaid;
 
 import java.util.Map;
+import java.util.List;
+import java.util.Collections;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
 
-import org.apereo.finaid.mvc.models.FinancialInfo;
+import org.apereo.finaid.mvc.models.*;
 import org.apereo.finaid.dao.IFinancialInfo;
-import org.apereo.finaid.mvc.models.ObjectFactory;
+//import org.apereo.finaid.mvc.models.ObjectFactory;
 
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ClassPathResource;
@@ -19,7 +21,7 @@ import org.springframework.stereotype.Service;
 public class MockStudentDao implements IFinancialInfo, InitializingBean{
   private FinancialInfo marshalMockStudent;
   private FinancialInfo unmarshalMockStudent; 
-  private static final ObjectFactory objectfactory = new ObjectFactory(); 
+  //  private static final ObjectFactory objectfactory = new ObjectFactory(); 
   public Resource mockData = new ClassPathResource("/mockdata/MockStudent.xml");
 
   public void setMockData(Resource mockData){
@@ -28,7 +30,7 @@ public class MockStudentDao implements IFinancialInfo, InitializingBean{
 
   @Override
   public void afterPropertiesSet() throws Exception {
-    JAXBContext jaxbContext = JAXBContext.newInstance(resources.mockdata.MockStudent.xml.class);
+    JAXBContext jaxbContext = JAXBContext.newInstance(org.apereo.finaid.mvc.models.FinancialInfo.class);
     Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
 
     this.unmarshalMockStudent = (FinancialInfo) unmarshaller.unmarshal(mockData.getInputStream());
@@ -36,22 +38,24 @@ public class MockStudentDao implements IFinancialInfo, InitializingBean{
 
   @Override
   public List<Award> getAwards(String termCode, long id){
-    return this.unmarshalMockStudent;
+    return this.unmarshalMockStudent.getAwards();
   }
 
   @Override
   public List<Hold> getHolds(String termCode, long id){
-    return this.unmarshalMockStudent;
+    return this.unmarshalMockStudent.getHolds();
   }
 
   @Override
   public Progress getProgress(String termCode, long id){
-    return this.unmarshalMockStudent;
+    return this.unmarshalMockStudent.getProgress();
   }
 
   @Override
   public List<String> getMessages(String termCode, long id){
-    return this.unmarshalMockStudent; 
+   //return this.unmarshalMockStudent.getMessage();
+   List<String> list = Collections.emptyList();
+   return list;
   }
 
 }
